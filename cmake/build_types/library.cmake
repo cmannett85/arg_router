8d431f0121d5cd2e253854a4e13122e652dbc720
Cam Mannett <camden.mannett@protonmail.ch>
2022-09-01 22:05:18 +0100

    Remove DOCS_ONLY option
    It's confusing and not very useful.  Replaced with an INSTALLTION_ONLY
    option that simply doesn't do any building or package finding.

--- cmake/build_types/library.cmake
+++ cmake/build_types/library.cmake
@@ -1,8 +1,11 @@
 ### Copyright (C) 2022 by Camden Mannett.  All rights reserved.
 
 add_library(arg_router INTERFACE ${HEADERS} ${FOR_IDE})
-add_dependencies(arg_router clangformat)
 target_compile_features(arg_router INTERFACE cxx_std_17)
 target_include_directories(arg_router
     INTERFACE "${CMAKE_SOURCE_DIR}/include"
 )
+
+if(NOT INSTALLATION_ONLY)
+    add_dependencies(arg_router clangformat)
+endif()
