1f4df211b3a7047816960bfb3a92d034a59cd2fb
Cam Mannett <camden.mannett@protonmail.ch>
2023-09-24 19:48:11 +0100

    #391 Move node name checking in tree_node to relevant policies
    WIP

--- include/arg_router/policy/long_name.hpp
+++ include/arg_router/policy/long_name.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include "arg_router/parsing/parse_target.hpp"
+#include "arg_router/parsing/parsing.hpp"
 #include "arg_router/policy/policy.hpp"
 
 namespace arg_router::policy
@@ -23,6 +25,9 @@ public:
     /** String type. */
     using string_type = S;
 
+    /** Policy priority. */
+    constexpr static auto priority = std::size_t{750};
+
     /** Constructor.
      *
      * @param str String instance
@@ -34,6 +39,39 @@ public:
      * @return Long name
      */
     [[nodiscard]] constexpr static std::string_view long_name() noexcept { return S::get(); }
+
+    /** Performs the expansion in the pre-parse phase.
+     *
+     * Checks if the token's first character matches the owning node's short name.  If there isn't a
+     * match or the owner does not have short name policy then it just returns false.  Otherwise all
+     * the characters in the token are converted into short form tokens, added to @a tokens.
+     *
+     * @note If a short-form expander is used, the long and short prefixes must be different
+     *
+     * @tparam ProcessedTarget @a processed_target payload type
+     * @tparam Parents Pack of parent tree nodes in ascending ancestry order
+     * @param tokens Currently processed tokens
+     * @param processed_target Previously processed parse_target of parent node, or empty is there
+     * is no non-root parent
+     * @param target Pre-parse generated target
+     * @param parents Parent node instances
+     * @return Always returns true because if the token doesn't match the short form name, the node
+     * may have a long form one that does.  No exception is stored in the return value
+     */
+    template <typename ProcessedTarget, typename... Parents>
+    [[nodiscard]] parsing::pre_parse_result pre_parse_phase(
+        parsing::dynamic_token_adapter& tokens,
+        [[maybe_unused]] utility::compile_time_optional<ProcessedTarget> processed_target,
+        [[maybe_unused]] parsing::parse_target& target,
+        [[maybe_unused]] const Parents&... parents) const
+    {
+        // The owner is named so there must be at least one token
+        if (tokens.empty()) {
+            return parsing::pre_parse_action::skip_node;
+        }
+
+        const auto token = *tokens.begin();
+    }
 
 private:
     static_assert(utility::utf8::count(long_name()) > 1,
